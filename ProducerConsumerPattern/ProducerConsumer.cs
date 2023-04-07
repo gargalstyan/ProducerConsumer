731b6774272fb7eeb5975406640e13eb7761f09e
Garik Galstyan <garikgalstyan8@gmail.com>
2023-04-08 02:35:52 +0400

    Add class

--- ProducerConsumerPattern/ProducerConsumer.cs
+++ ProducerConsumerPattern/ProducerConsumer.cs
@@ -8,5 +8,97 @@ namespace ProducerConsumerPattern
 {
     internal class ProducerConsumer
     {
+        Queue<int> buffer = new Queue<int>();
+        List<Thread> producerThreads = new List<Thread>();
+        List<Thread> consumerThreads = new List<Thread>();
+        private readonly object o = new object();
+        public int ProducerCount { get; }
+        public int ConsumerCount { get; }
+        public ProducerConsumer(int producerCount, int consumerCount)
+        {
+            ProducerCount = producerCount;
+            ConsumerCount = consumerCount;
+        }
+        public void Run()
+        {
+            CreateThreads();
+            foreach (var item in producerThreads)
+            {
+                item.Start();
+            }
+            foreach (var item in consumerThreads)
+            {
+                item.Start();
+            }
+        }
+        public void CreateThreads()
+        {
+            for (int i = 0; i < ProducerCount; i++)
+            {
+                int index = i;
+                producerThreads.Add(new Thread(() => Producer()));
+                producerThreads[i].Name = $"ThreadProducer-{i}";
+            }
+            for (int i = 0; i < ConsumerCount; i++)
+            {
+                consumerThreads.Add(new Thread(() => Consumer()));
+                consumerThreads[i].Name = $"ThreadConsumer-{i}";
+            }
+        }
+        public void Producer()
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                lock (o)
+                {
+                    int number = rnd.Next(0, 100);
+                    buffer.Enqueue(number);
+                    Console.WriteLine($"{Thread.CurrentThread.Name} {number}");
+                    if (buffer.Count >= 50)
+                    {
+                        Console.WriteLine("Queue is full!!!!!");
+                        Monitor.PulseAll(o);
+                        Monitor.Wait(o);
+                    }
+                  
+                    Thread.Sleep(rnd.Next(0, 1000));
+                }
+            }
+        }
+        public void Consumer()
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                lock (o)
+                {
+                    if (buffer.Count < 40)
+                    {
+                        Monitor.PulseAll(o);
+                    }
+                    if (buffer.Count == 0)
+                    {
+                        Monitor.PulseAll(o);
+                        Monitor.Wait(o);
+                    }
+                    if (buffer.Count >= 50)
+                    {
+                        while (buffer.Count >= 40)
+                        {
+                            int number = buffer.Dequeue();
+                            Console.WriteLine($"{Thread.CurrentThread.Name} {number}");
+                        }
+                    }
+                    else
+                    {
+                        int number = buffer.Dequeue();
+                        Console.WriteLine($"{Thread.CurrentThread.Name} {number}");
+                    }
+                    Thread.Sleep(rnd.Next(0, 1000));
+                }
+
+            }
+        }
     }
 }
--- ProducerConsumerPattern/Program.cs
+++ ProducerConsumerPattern/Program.cs
@@ -1,55 +1,57 @@
-﻿
+﻿using ProducerConsumerPattern;
+ProducerConsumer producerConsumer = new ProducerConsumer(10,3);
+producerConsumer.Run();
 
-Queue<int> buffer = new Queue<int>();
-object o = new object();
-void Producer()
-{
-    Random rnd = new Random();
-    while (true)
-    {
-        lock (o)
-        {
-            int number = rnd.Next(0, 100);
-            buffer.Enqueue(number);
-            Console.WriteLine($"Producer thread {number}");
-            if (buffer.Count > 100)
-            {
-                Monitor.Pulse(o);
-                Monitor.Wait(o);
-            }
-            Thread.Sleep(rnd.Next(0, 500));
-        }
+//Queue<int> buffer = new Queue<int>();
+//object o = new object();
+//void Producer()
+//{
+//    Random rnd = new Random();
+//    while (true)
+//    {
+//        lock (o)
+//        {
+//            int number = rnd.Next(0, 100);
+//            buffer.Enqueue(number);
+//            Console.WriteLine($"Producer thread {number}");
+//            if (buffer.Count > 100)
+//            {
+//                Monitor.Pulse(o);
+//                Monitor.Wait(o);
+//            }
+//            Thread.Sleep(rnd.Next(0, 500));
+//        }
 
-    }
-}
-void Consumer()
-{
-    Random rnd = new Random();
-    while (true)
-    {
-        lock (o)
-        {
-            if (buffer.Count < 90)
-            {
-                Monitor.Pulse(o);
-            }
-            if (buffer.Count == 0)
-            {
-                Monitor.Pulse(o);
-                Monitor.Wait(o);
-            }
+//    }
+//}
+//void Consumer()
+//{
+//    Random rnd = new Random();
+//    while (true)
+//    {
+//        lock (o)
+//        {
+//            if (buffer.Count < 90)
+//            {
+//                Monitor.Pulse(o);
+//            }
+//            if (buffer.Count == 0)
+//            {
+//                Monitor.Pulse(o);
+//                Monitor.Wait(o);
+//            }
 
-            int number = buffer.Dequeue();
-            Console.WriteLine($"Consumer Thread {number}");
-            Thread.Sleep(rnd.Next(0, 500));
-        }
+//            int number = buffer.Dequeue();
+//            Console.WriteLine($"Consumer Thread {number}");
+//            Thread.Sleep(rnd.Next(0, 500));
+//        }
 
-    }
-}
-Thread thread = new Thread(() => Producer());
-Thread thread1 = new Thread(() => Consumer());
-thread.Start();
-thread1.Start();
-thread1.Join();
-thread.Join();
+//    }
+//}
+//Thread thread = new Thread(() => Producer());
+//Thread thread1 = new Thread(() => Consumer());
+//thread.Start();
+//thread1.Start();
+//thread1.Join();
+//thread.Join();
 
